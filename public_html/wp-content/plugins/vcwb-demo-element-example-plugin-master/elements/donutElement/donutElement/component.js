c4ec2d18d4aa02751a81cd707faec71d8a6e4728
Michael DaSilva <programmedmikey@gmail.com>
2022-02-09 17:07:53 +0000

    creating donut component

--- public_html/wp-content/plugins/vcwb-demo-element-example-plugin-master/elements/donutElement/donutElement/component.js
+++ public_html/wp-content/plugins/vcwb-demo-element-example-plugin-master/elements/donutElement/donutElement/component.js
@@ -1,34 +1,77 @@
 import React from 'react'
 import { getService } from 'vc-cake'
+import classNames from 'classnames'
+import { renderToStaticMarkup } from 'react-dom/server'
+
+// Import Donut component for each chart option
+// Attributes for each donut chart are taken from paramsGroup attribute type in settings.json
+import Donut from './donut'
 
 const vcvAPI = getService('api')
 
-export default class ElementBoilerplate extends vcvAPI.elementComponent {
+export default class VerticalChart extends vcvAPI.elementComponent {
+  // Generate an array of JSX with donut chart graph
+  getContent () {
+    const { bars } = this.props.atts
+
+    const items = bars.value ? bars.value : bars
+    return items.map((item, i) => {
+      const key = `bar-${this.props.id}-${i}`
+      const attributes = item
+      attributes.atts = {}
+      attributes.atts.tag = this.props.atts.tag
+      attributes.atts.title = item.title
+      attributes.atts.text = item.text
+      attributes.atts.value = item.value
+      attributes.itemIndex = i
+      attributes.getMixin = this.getInnerMixinData.bind(this, 'bars')
+      return <Donut {...attributes} key={key} />
+    })
+  }
+
   render () {
     const { id, atts, editor } = this.props
-    const { output, customClass, metaCustomId } = atts // destructuring assignment for attributes from settings.json with access public
-    let elementBoilerplateClasses = 'vce-element-boilerplate'
-    const wrapperClasses = 'vce-element-boilerplate-wrapper vce'
+    const { toggleValue, customClass, metaCustomId } = atts
+    let containerClasses = ['vce', 'vce-vertical-charts-container']
     const customProps = {}
-    if (typeof customClass === 'string' && customClass) {
-      elementBoilerplateClasses = elementBoilerplateClasses.concat(' ' + customClass)
-    }
 
-    const mixinData = this.getMixinData('boilerplateColorMixin')
-    if (mixinData) {
-      elementBoilerplateClasses = elementBoilerplateClasses.concat(` vce-element-boilerplate--color-${mixinData.selector}`)
+    if (typeof customClass === 'string' && customClass) {
+      containerClasses.push(customClass)
     }
 
     if (metaCustomId) {
       customProps.id = metaCustomId
     }
 
-    const doAll = this.applyDO('all')
+    if (toggleValue) {
+      containerClasses.push('vce-vertical-chart-display-value')
+    }
 
+    let mixinData = this.getMixinData('barThickness')
+    if (mixinData) {
+      containerClasses.push(`vce-vertical-chart-thickness--${mixinData.selector}`)
+    }
+
+    mixinData = this.getMixinData('titleColor')
+    if (mixinData) {
+      containerClasses.push(`vce-vertical-chart-title-color--${mixinData.selector}`)
+    }
+
+    containerClasses = classNames(containerClasses)
+
+    const doAll = this.applyDO('all')
+    const content = this.getContent()
+
+    // Content is saved as a String with raw HTML
+    const barsHTML = renderToStaticMarkup(content)
+
+    // Need to use wrapper with .vcvhelper to save raw HTML string in data attribute for View Page
     return (
-      <div className={elementBoilerplateClasses} {...editor} {...customProps}>
-        <div className={wrapperClasses} id={'el-' + id} {...doAll}>
-          {output}
+      <div {...editor} id={`el-${id}`} className={containerClasses} {...doAll}>
+        <div className='vce-vertical-charts' {...customProps}>
+          <div className='vcvhelper' data-vcvs-html={barsHTML}>
+            {content}
+          </div>
         </div>
       </div>
     )
